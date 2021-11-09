8060204458e6b5134d96def0e27a052def4fb5c8
danielguirra <daniel.guirra777@gmail.com>
2021-11-09 09:09:12 -0300

    ts

--- food.js
+++ food.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Food = void 0;
 const axios_1 = __importDefault(require("axios"));
 /**
  * Digite parametro da comida exemplo "batata"
